c51870c53549a574b1af6bcb92b84e79248e4e19
TRUNG <trung.tran24@hcmut.edu.vn>
2023-10-01 22:26:31 +0700

    init for ex 7

---
+++ GIT_LAB_2/Core/Inc/software_timer.h
@@ -0,0 +1,14 @@
+/*
+ * software_timer.h
+ *
+ *
+ */
+
+#ifndef INC_SOFTWARE_TIMER_H_
+#define INC_SOFTWARE_TIMER_H_
+
+extern int timer1_flag;
+void setTimer1(int duration);
+void timerRun();
+
+#endif /* INC_SOFTWARE_TIMER_H_ */
